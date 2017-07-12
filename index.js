e7ad21d93ba922627a67de1c5344ab7104e5d6f5
menyan <albeemen@gmail.com>
2017-07-12 16:09:00 -0700

    a2

--- index.js
+++ index.js
@@ -1,36 +1,90 @@
-//Yan Men
-//ITC230
-//index.js
+var http = require("http"),
+    qs = require("querystring"),
+    books = require("./lib/books.js"),
+    fs = require("fs");
 
-var http = require('http'), fs = require('fs');
+const LISTEN_PORT = 3000;
 
-function serveStatic(res, path, contentType, responseCode) {
-  if (!responseCode) responseCode = 200;
-  console.log(__dirname + path)
-  fs.readFile(__dirname + path, function(err, data) {
-      if(err) {
-        res.writeHead(500, {'Content-Type': 'text/plain'});
-        res.end('Internal Server Error');
-      } else {
-        res.writeHead(responseCode, {'Content-Type': contentType});
-        res.end(data);
-      }
-  });
+/**
+ * Serve a static file
+ * @param object res = HTTP response
+ * @param string path = path to file
+ * @param string contentType
+ * @param int responseCode
+ */
+function serveStaticFile(res, path, contentType, responseCode) {
+    if(!responseCode) responseCode = 200;
+    fs.readFile(__dirname + path, function(err, data) {
+       if(err)  {
+           res.writeHead(500, { 'Content-Type': 'text/plain' });
+           res.end('500 - Internal Error');
+       } else {
+           res.writeHead(responseCode, { 'Content-Type': contentType });
+           res.end(data, 'utf-8');
+       } 
+     });
 }
 
-http.createServer(function(req, res) {
-	var path = req.url.toLowerCase();
-	switch (path) {
-		case '/':
-			serveStatic(res, '/public/home.html', 'text/html');
-			break;
-		case '/about':
-      		res.writeHead(200, { 'Content-Type': 'text/plain' });
-            res.end('Yan Men-ITC230 SU17 Advanced JavaSript');
+/**
+ * Create server
+ * @param object req
+ * @param object res
+ */
+http.createServer(function(req,res) {
+    // build url, path and params object
+    let url = req.url.split("?"); // split the request URL
+    let params = qs.parse(url[1]); // create params
+    let path = url[0].toLowerCase(); // establish lowercased path
+   
+    switch(path) {
+
+        case '/':
+            serveStaticFile(res, '/public/home.html', 'text/html');
             break;
-		default:
-			res.writeHead(404, {'Content-Type': 'text/plain'});
-      		res.end('404:Page not found.');
+
+        case '/about':
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            res.end('ITC230 SU17 Advanced JavaSript\nApp Name: bookknow\nVersion : 1.0.0\n');
             break;
-	}
-}).listen(3000);
+            
+        case '/getall':
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            res.end(books.byTitleAsc());
+            break;
+            
+        case '/get':
+            if (params.title) {
+                res.writeHead(200, { 'Content-Type': 'text/plain' });
+                res.end(books.get(params.title));
+            } else {
+                res.writeHead(422, { 'Content-Type': 'text/plain' });
+                res.end('422 Error - get request requires title parameter');
+            }
+            break;
+            
+        case '/add':
+            if (params.title) {
+                res.writeHead(200, { 'Content-Type': 'text/plain' });
+                res.end(books.add(params));
+            } else {
+                res.writeHead(422, { 'Content-Type': 'text/plain' });
+                res.end('422 Error - add request requires at least title parameter');
+            }
+            break;
+            
+        case '/delete':
+            if (params.title) {
+                res.writeHead(200, { 'Content-Type': 'text/plain' });
+                res.end(books.delete(params.title));
+            } else {
+                res.writeHead(422, { 'Content-Type': 'text/plain' });
+                res.end('422 Error - delete request requires title parameter');
+            }
+            break;
+
+        default:
+            res.writeHead(404, { 'Content-Type': 'text/plain' });
+            res.end('404 Error - Resource Not Found: ' + path);
+            break;
+    }
+}).listen(process.env.PORT || LISTEN_PORT);
